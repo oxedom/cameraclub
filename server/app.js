8247fa01f7d1de10efaf6674c6a02a3e5f483848
SamLinux <oxedom@gmail.com>
2022-04-26 13:36:45 +0300

    setting up server

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const port = 4000
 const app = express()
 const cors = require('cors')
 const config = require('./config/config')
-const user_routes = require('./Routers/users')
+const user_routes = require('./routers/users')
+
 // const post_routes = require('./Routers/posts')
 
 //KEVINS WORK
@@ -37,7 +38,4 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/user/tasks', task_routes)
-app.use('/api/user/posts', post_routes)
 app.use('/api/user/', user_routes)
-// app.listen(3000);
