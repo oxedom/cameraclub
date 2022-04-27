717fac5cf4a84964150b3a32efc2eb36ee7c8ba8
SamLinux <oxedom@gmail.com>
2022-04-27 13:10:26 +0300

    Added Mutation logic for adding posts and comments One Too many!

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -5,7 +5,8 @@ const commentSchema = mongoose.Schema(
         userid: {type: String},
         text: { type: String},
         likes: { type: Number},
-        date: { type: Number}
+        date: { type: Number},
+        postid: { type: String}
     }
 )
 
